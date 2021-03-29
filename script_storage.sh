ab6e44cf54faad38cc7a0e4f2ccdb5871d418215
sergio2526 <sergiorubiano25@hotmail.com>
2021-03-29 17:25:51 -0500

    script storage

---
+++ script_storage.sh
@@ -0,0 +1,6 @@
+
+printf "\n1) Subiendo archivo carpeta images"
+gsutil cp -r /home/sergio/Documentos/Repositorios/Rice-Project/images NAME_BUCKET
+
+printf "\n2) Subiendo archivos carpeta img_recortadas"
+gsutil cp -r /home/sergio/Documentos/Repositorios/Rice-Project/img_recortadas NAME_BUCKET
